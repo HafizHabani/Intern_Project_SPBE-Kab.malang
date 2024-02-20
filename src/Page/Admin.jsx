b88afa1130681a9f0c7bb19e570deecb3494cf44
Hamdani Zulva <hamdanizulva0@webmail.umm.ac.id>
2024-02-20 10:21:03 +0700

    modified:   src/Page/Admin.jsx 	modified:   src/components/TabelDomain.jsx

--- src/Page/Admin.jsx
+++ src/Page/Admin.jsx
@@ -2,11 +2,25 @@ import React from 'react'
 import { logokab } from '../assets'
 import {
     Card,
+    Typography,
     List,
     ListItem,
+    ListItemPrefix,
+    Accordion,
+    AccordionHeader,
+    AccordionBody,
+    
   } from "@material-tailwind/react";
+  import { ChevronRightIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
+import { KebijakanDummy } from '../constants';
+import TabelDomain from '../components/TabelDomain';
 
 const Admin = () => {
+  const [open, setOpen] = React.useState(0);
+ 
+  const handleOpen = (value) => {
+    setOpen(open === value ? 0 : value);
+  };
   return (
     <div>
       <div className='px-10 shadow-lg'>
@@ -15,32 +29,68 @@ const Admin = () => {
         </nav>
       </div>
       <div className='flex flex-row'>
-        <div>
+        <div className='bg-gradient-to-br from-emerald-500 from-5% to-white to-95%'>
             <Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
             <List className='flex justify-center'>
-                <ListItem className='px-10 pt-5 font-bold'>
-                Dashboard
-                </ListItem >
-                <ListItem className='px-10 pt-5 font-bold'>
-                E-Commerce
+            <Accordion
+                open={open === 1}
+                icon={
+                    <ChevronDownIcon
+                    strokeWidth={2.5}
+                    className={`mx-auto h-4 w-4 transition-transform ${open === 1 ? "rotate-180" : ""}`}
+                    />
+                }
+                >
+                <ListItem className="p-0" selected={open === 1}>
+                    <AccordionHeader onClick={() => handleOpen(1)} className="border-b-0 px-10 py-0 pt-4  font-bold">
+                    <Typography color="blue-gray" className="">
+                        Domain
+                    </Typography>
+                    </AccordionHeader>
                 </ListItem>
-                <ListItem className='px-10 pt-5 font-bold'>
-                Inbox
+                <AccordionBody className="py-0 ml-8">
+                    <List className="p-0">
+                    <ListItem>
+                        <ListItemPrefix>
+                        <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                        </ListItemPrefix>
+                        Kebijakan
+                    </ListItem>
+                    <ListItem>
+                        <ListItemPrefix>
+                        <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                        </ListItemPrefix>
+                        Manajemen
+                    </ListItem>
+                    <ListItem>
+                        <ListItemPrefix>
+                        <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                        </ListItemPrefix>
+                        Layanan
+                    </ListItem>
+                    <ListItem>
+                        <ListItemPrefix>
+                        <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                        </ListItemPrefix>
+                        Tata Kelola
+                    </ListItem>
+                    </List>
+                </AccordionBody>
+                </Accordion>
+                <ListItem className='px-10 pt-4 font-bold'>
+                Berita
                 </ListItem>
-                <ListItem className='px-10 pt-5 font-bold'>
-                Profile
+                <ListItem className='px-10 pt-4 font-bold'>
+                Galeri 
                 </ListItem>
-                <ListItem className='px-10 pt-5 font-bold'>
-                Settings
-                </ListItem>
-                <ListItem className='px-10 pt-5 font-bold'>
+                <ListItem className='px-10 pt-4 font-bold text-red-600'>
                 Log Out
                 </ListItem>
             </List>
             </Card>
         </div>
         <div className=''>
-            
+          <TabelDomain domain = {KebijakanDummy} keterangan = "Kebijakan"/>
         </div>
       </div>
     </div>
--- src/components/TabelDomain.jsx
+++ src/components/TabelDomain.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
 
-const TabelDomain = () => {
+const TabelDomain = (prop) => {
   return (
-    <div>
-      
+    <div className='p-4'>
+      <div className='flex flex-row justify-between items-center py-2'>
+        <p className='font-bold text-2xl'>
+          Tabel Domain {prop.keterangan}
+        </p>
+        <button className='btn btn-sm bg-primary text-white'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+          </svg>
+          Tambah Indikator
+        </button>
+      </div>
+      <table className="table border-solid border-2">
+        <thead className='text-lg text-primary border-solid border-2'>
+          <tr>
+            <th className='border-solid border-2'>No</th>
+            <th className='border-solid border-2'>Aspek</th>
+            <th className='border-solid border-2'>Indikator</th>
+            <th className='border-solid border-2'>Penjelasan</th>
+            <th className='border-solid border-2'>Action</th>
+          </tr>
+        </thead>
+        <tbody className='border-solid border-2'>
+          {prop.domain.map((kebijakan)=>(
+            <tr  key={kebijakan.nomor}>
+              <td className='border-solid border-2'>{kebijakan.nomor}</td>
+              <td className='border-solid border-2'>{kebijakan.aspek}</td>
+              <td className='border-solid border-2'>{kebijakan.indikator}</td>
+              <td className='border-solid border-2'>
+                {kebijakan.penjelasan}<br/> 
+                <a href={kebijakan.linkDoc} className='text-primary'>Selengkapnya</a>
+                </td>
+              <td>
+                <div className='flex flex-row justify-center items-center '>
+                  <button className='btn btn-square bg-emerald-400'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+                    </svg>
+                  </button>
+                  <button className='btn btn-square bg-red-500'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-6 h-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                    </svg>
+                  </button>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
