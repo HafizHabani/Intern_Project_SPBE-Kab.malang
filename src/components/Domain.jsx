47eebe88020037d291a8123c95b77bb94961cc23
HafizHabani <hafizhbni@gmail.com>
2024-03-01 14:55:29 +0700

    Update 1 Maret 2024

--- src/components/Domain.jsx
+++ src/components/Domain.jsx
@@ -1,26 +1,30 @@
 import { useState } from 'react';
 import { kebijakan, layanan, kelola, manajemen } from '../assets';
 import { Link } from 'react-router-dom';
-import { kebijakanDummy,manajemenDummy,kelolaDummy } from '../constants';
+import { kebijakanDummy, manajemenDummy, kelolaDummy, layananDummy } from '../constants';
 
 
 
-const Domain = () =>  {
+const Domain = () => {
   const [KebijakanIsHiden, setKebijakanIsHiden] = useState(true);
   const [KelolaIsHiden, setKelolaIsHiden] = useState(true);
+  const [LayananIsHiden, setLayananIsHiden] = useState(true);
   const [ManajemenIsHiden, setManajemenIsHiden] = useState(true);
 
   const handleShowKebijakan = () => setKebijakanIsHiden(false);
   const handleShowKelola = () => setKelolaIsHiden(false);
+  const handleShowLayanan = () => setLayananIsHiden(false);
   const handleShowManajemen = () => setManajemenIsHiden(false);
 
-  const handleHideKebijakan = () => setKebijakanIsHiden(true); 
-  const handleHideKelola = () => setKelolaIsHiden(true); 
-  const handleHideManajemen = () => setManajemenIsHiden(true); 
+  const handleHideKebijakan = () => setKebijakanIsHiden(true);
+  const handleHideKelola = () => setKelolaIsHiden(true);
+  const handleHideLayanan = () => setLayananIsHiden(true);
+  const handleHideManajemen = () => setManajemenIsHiden(true);
 
-  const KebijakanPressed = () =>{
+  const KebijakanPressed = () => {
     if (KebijakanIsHiden === true) {
       handleShowKebijakan();
+      handleHideLayanan();
       handleHideKelola();
       handleHideManajemen();
     }
@@ -29,9 +33,22 @@ const Domain = () =>  {
     }
   }
 
-  const KelolaPressed = () =>{
+  const LayananPressed = () => {
+    if (LayananIsHiden === true) {
+      handleShowLayanan();
+      handleHideKebijakan()
+      handleHideKelola();
+      handleHideManajemen();
+    }
+    else {
+      handleHideLayanan();
+    }
+  }
+
+  const KelolaPressed = () => {
     if (KelolaIsHiden === true) {
       handleShowKelola();
+      handleHideLayanan();
       handleHideKebijakan();
       handleHideManajemen();
     } else {
@@ -39,9 +56,10 @@ const Domain = () =>  {
     }
   }
 
-  const ManajemenPressed = () =>{
+  const ManajemenPressed = () => {
     if (ManajemenIsHiden === true) {
       handleShowManajemen();
+      handleHideLayanan();
       handleHideKebijakan();
       handleHideKelola();
     } else {
@@ -49,135 +67,170 @@ const Domain = () =>  {
     }
   }
 
-  return(
-  <div className='py-8'>
-    <div className='flex justify-center font-semibold text-lg text-center mb-5'>
-      Domain Yang Tersedia Dalam SPBE Kabupaten Malang
-    </div>
-    <div className='flex flex-wrap justify-center sm:justify-around md:justify-between lg:justify-around xl:justify-between sm:mx-10 md:mx-20'>
-      <button onClick={KebijakanPressed} className="btn w-full text-white sm:w-48 md:w-40 lg:w-48 xl:w-56 btn-lg bg-gradient-to-t from-rose-900 via-red-700 to-red-500 mb-4 md:mb-0 md:mr-4 lg:mr-0">
-        <img src={kebijakan} alt='none' className="w-10 h-10 sm:w-auto sm:h-auto" />
-        Kebijakan
-      </button>
-      <button onClick={KelolaPressed} className="btn text-white w-full sm:w-48 md:w-40 lg:w-48 xl:w-56 btn-lg bg-gradient-to-t from-rose-900 via-red-700 to-red-500 mb-4 md:mb-0 md:mr-4 lg:mr-0">
-        <img src={kelola} alt='none' className="w-10 h-10 sm:w-auto sm:h-auto " />
-        Tata Kelola
-      </button>
-      <button onClick={ManajemenPressed} className="btn text-white w-full sm:w-48 md:w-40 lg:w-48 xl:w-56 btn-lg bg-gradient-to-t from-rose-900 via-red-700 to-red-500 mb-4 md:mb-0 md:mr-4 lg:mr-0">
-        <img src={manajemen} alt='none' className="w-10 h-10 sm:w-auto sm:h-auto " />
-        Manajemen
-      </button>
-      <Link to="/Layanan" className='w-full sm:w-48 md:w-40 lg:w-48 xl:w-56'>
-      <button className="btn w-full text-white sm:w-48 md:w-40 lg:w-48 xl:w-56 btn-lg bg-gradient-to-t from-rose-900 via-red-700 to-red-500 mb-4 md:mb-0 md:mr-4 lg:mr-0">
-        <img src={layanan} alt='none' className="w-10 h-10 sm:w-auto sm:h-auto " />
-        Layanan
-      </button>
-      </Link>
-    </div>
-    
-    <div className={`${KebijakanIsHiden? 'hidden' : ''} my-10 `} >
-      <div className="relative">
+  return (
+    <div className='py-8'>
+      <div className='flex justify-center font-semibold text-lg text-center mb-5'>
+        Domain Yang Tersedia Dalam SPBE Kabupaten Malang
       </div>
-      <table className="table">
-        <thead className='text-lg text-primary'>
-          <tr>
-            <th>No</th>
-            <th>Aspek</th>
-            <th>Indikator</th>
-            <th>Penjelasan</th>
-          </tr>
-        </thead>
-        <tbody>
-          {kebijakanDummy.data.data.map((kebijakan, index)=>(
-            <tr key={kebijakan.id}>
-              <td>{index+1}</td>
-              <td>{kebijakan.aspek}</td>
-              <td>{kebijakan.indikator}</td>
-              <td>
-                {kebijakan.penjelasan}<br/> 
-                <a href={kebijakan.link} className='text-primary'>Selengkapnya</a>
-                </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <div className="join flex justify-center mt-5">
-        <button className="join-item btn btn-sm bg-primary border-red-700 hover:bg-red-900 text-white">«</button>
-        <button className="join-item btn btn-sm bg-primary border-red-700 hover:bg-red-900 text-white">Page 1</button>
-        <button className="join-item btn btn-sm bg-primary border-red-700 hover:bg-red-900 text-white">»</button>
+      <div className='flex flex-wrap justify-center sm:justify-around md:justify-between lg:justify-around xl:justify-between sm:mx-10 md:mx-20'>
+        <button onClick={KebijakanPressed} className="btn w-full text-white sm:w-48 md:w-40 lg:w-48 xl:w-56 btn-lg bg-gradient-to-t from-rose-900 via-red-700 to-red-500 mb-4 md:mb-0 md:mr-4 lg:mr-0">
+          <img src={kebijakan} alt='none' className="w-10 h-10 sm:w-auto sm:h-auto" />
+          Kebijakan
+        </button>
+        <button onClick={KelolaPressed} className="btn text-white w-full sm:w-48 md:w-40 lg:w-48 xl:w-56 btn-lg bg-gradient-to-t from-rose-900 via-red-700 to-red-500 mb-4 md:mb-0 md:mr-4 lg:mr-0">
+          <img src={kelola} alt='none' className="w-10 h-10 sm:w-auto sm:h-auto " />
+          Tata Kelola
+        </button>
+        <button onClick={ManajemenPressed} className="btn text-white w-full sm:w-48 md:w-40 lg:w-48 xl:w-56 btn-lg bg-gradient-to-t from-rose-900 via-red-700 to-red-500 mb-4 md:mb-0 md:mr-4 lg:mr-0">
+          <img src={manajemen} alt='none' className="w-10 h-10 sm:w-auto sm:h-auto " />
+          Manajemen
+        </button>
+        <button onClick={LayananPressed} className="btn w-full text-white sm:w-48 md:w-40 lg:w-48 xl:w-56 btn-lg bg-gradient-to-t from-rose-900 via-red-700 to-red-500 mb-4 md:mb-0 md:mr-4 lg:mr-0">
+          <img src={layanan} alt='none' className="w-10 h-10 sm:w-auto sm:h-auto " />
+          Layanan
+        </button>
       </div>
-    </div>
 
-    <div className={`${ManajemenIsHiden? 'hidden' : ''} my-10 `}>
-      <div className="relative">
-       
-      </div>
-      <table className="table ">
-        <thead className='text-lg text-primary'>
-          <tr>
-            <th>No</th>
-            <th>Aspek</th>
-            <th>Indikator</th>
-            <th>Penjelasan</th>
-          </tr>
-        </thead>
-        <tbody>
-          {manajemenDummy.data.data.map((manajemen, index)=>(
-            <tr key={manajemen.id}>
-              <td>{index+1}</td>
-              <td>{manajemen.aspek}</td>
-              <td>{manajemen.indikator}</td>
-              <td>
-                {manajemen.penjelasan}<br/> 
-                <a href={manajemen.link} className='text-primary'>Selengkapnya</a>
-                </td>
+      <div className={`${KebijakanIsHiden ? 'hidden' : ''} my-10 `} >
+        <div className="relative">
+        </div>
+        <table className="table">
+          <thead className='text-lg text-primary'>
+            <tr>
+              <th>No</th>
+              <th>Aspek</th>
+              <th>Indikator</th>
+              <th>Penjelasan</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-      <div className="join flex justify-center mt-5">
-        <button className="join-item btn btn-sm bg-primary border-red-700 hover:bg-red-900 text-white">«</button>
-        <button className="join-item btn btn-sm bg-primary border-red-700 hover:bg-red-900 text-white">Page 1</button>
-        <button className="join-item btn btn-sm bg-primary border-red-700 hover:bg-red-900 text-white">»</button>
-      </div>
-    </div>
-
-    <div className={`${KelolaIsHiden? 'hidden' : ''} my-10`}>
-    <div className="relative">
-       
-      </div>
-      <table className="table ">
-        <thead className='text-lg text-primary'>
-          <tr>
-            <th>No</th>
-            <th>Aspek</th>
-            <th>Indikator</th>
-            <th>Penjelasan</th>
-          </tr>
-        </thead>
-        <tbody>
-          {kelolaDummy.data.data.map((kelola, index)=>(
-            <tr key={kelola.id}>
-              <td>{index+1}</td>
-              <td>{kelola.aspek}</td>
-              <td>{kelola.indikator}</td>
-              <td>
-                {kelola.penjelasan}<br/> 
-                <a href={kelola.link} className='text-primary'>Selengkapnya</a>
+          </thead>
+          <tbody>
+            {kebijakanDummy.data.data.map((kebijakan, index) => (
+              <tr key={kebijakan.id}>
+                <td>{index + 1}</td>
+                <td>{kebijakan.aspek}</td>
+                <td>{kebijakan.indikator}</td>
+                <td>
+                  {kebijakan.penjelasan}<br />
+                  <a href={kebijakan.link} className='text-primary'>Selengkapnya</a>
                 </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <div className="join flex justify-center mt-5">
-        <button className="join-item btn btn-sm bg-primary border-red-700 hover:bg-red-900 text-white">«</button>
-        <button className="join-item btn btn-sm bg-primary border-red-700 hover:bg-red-900 text-white">Page 1</button>
-        <button className="join-item btn btn-sm bg-primary border-red-700 hover:bg-red-900 text-white">»</button>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className="join flex justify-center mt-5">
+          <button className="join-item btn btn-sm bg-primary border-red-700 hover:bg-red-900 text-white">«</button>
+          <button className="join-item btn btn-sm bg-primary border-red-700 hover:bg-red-900 text-white">Page 1</button>
+          <button className="join-item btn btn-sm bg-primary border-red-700 hover:bg-red-900 text-white">»</button>
+        </div>
       </div>
-    </div>
 
-  </div>
+      <div className={`${ManajemenIsHiden ? 'hidden' : ''} my-10 `}>
+        <div className="relative">
+
+        </div>
+        <table className="table ">
+          <thead className='text-lg text-primary'>
+            <tr>
+              <th>No</th>
+              <th>Aspek</th>
+              <th>Indikator</th>
+              <th>Penjelasan</th>
+            </tr>
+          </thead>
+          <tbody>
+            {manajemenDummy.data.data.map((manajemen, index) => (
+              <tr key={manajemen.id}>
+                <td>{index + 1}</td>
+                <td>{manajemen.aspek}</td>
+                <td>{manajemen.indikator}</td>
+                <td>
+                  {manajemen.penjelasan}<br />
+                  <a href={manajemen.link} className='text-primary'>Selengkapnya</a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className="join flex justify-center mt-5">
+          <button className="join-item btn btn-sm bg-primary border-red-700 hover:bg-red-900 text-white">«</button>
+          <button className="join-item btn btn-sm bg-primary border-red-700 hover:bg-red-900 text-white">Page 1</button>
+          <button className="join-item btn btn-sm bg-primary border-red-700 hover:bg-red-900 text-white">»</button>
+        </div>
+      </div>
+
+      <div className={`${KelolaIsHiden ? 'hidden' : ''} my-10`}>
+        <div className="relative">
+
+        </div>
+        <table className="table ">
+          <thead className='text-lg text-primary'>
+            <tr>
+              <th>No</th>
+              <th>Aspek</th>
+              <th>Indikator</th>
+              <th>Penjelasan</th>
+            </tr>
+          </thead>
+          <tbody>
+            {kelolaDummy.data.data.map((kelola, index) => (
+              <tr key={kelola.id}>
+                <td>{index + 1}</td>
+                <td>{kelola.aspek}</td>
+                <td>{kelola.indikator}</td>
+                <td>
+                  {kelola.penjelasan}<br />
+                  <a href={kelola.link} className='text-primary'>Selengkapnya</a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className="join flex justify-center mt-5">
+          <button className="join-item btn btn-sm bg-primary border-red-700 hover:bg-red-900 text-white">«</button>
+          <button className="join-item btn btn-sm bg-primary border-red-700 hover:bg-red-900 text-white">Page 1</button>
+          <button className="join-item btn btn-sm bg-primary border-red-700 hover:bg-red-900 text-white">»</button>
+        </div>
+      </div>
+
+
+      <div className={`${LayananIsHiden ? 'hidden' : ''} my-10`}>
+        <div>
+          <h6 className="text-lg font-bold border-4 border-solid border-white border-b-primary py-5 pe-40">Layanan Kabupaten Malang</h6>
+          <div className='pt-10 flex flex-col'>
+            <div className='flex flex-wrap justify-around'>
+              {layananDummy.data.map((layanan, index) => (
+                <div key={index} className='flex flex-row items-center justify-between border-4 border-solid border-gray-300 rounded-lg mt-5 p-4 w-96'>
+                  <div className='flex flex-row'>
+                    <img src={layanan.logo} alt="LogoKabMal" className="flex max-w-sm rounded-lg" style={{ maxHeight: "80px", maxWidth: "40px" }} />
+                    <h5 className='ml-3 font-bold text-sm'>{layanan.nama}</h5>
+                  </div>
+                  <div className='flex flex-col items-center'>
+                    <button className="btn px-6 btn-sm text-sm text-white  btn-primary" onClick={() => document.getElementById('my_modal_2').showModal()}>Detail</button>
+                    <dialog id="my_modal_2" className="modal">
+                      <div className="modal-box">
+                        <h3 className="font-bold text-lg">{layanan.nama}</h3>
+                        <p className="py-4">Lorem Ipsumnya Rysa Bermain Kuda Di Rumah Irfan</p>
+                      </div>
+                      <form method="dialog" className="modal-backdrop">
+                        <button>close</button>
+                      </form>
+                    </dialog>
+                    <a href={layanan.link} className="mt-3 btn btn-sm text-sm text-white btn-primary">Kunjungi</a>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="join flex justify-center mt-5">
+          <button className="join-item btn btn-sm bg-primary border-red-700 hover:bg-red-900 text-white">«</button>
+          <button className="join-item btn btn-sm bg-primary border-red-700 hover:bg-red-900 text-white">Page 1</button>
+          <button className="join-item btn btn-sm bg-primary border-red-700 hover:bg-red-900 text-white">»</button>
+        </div>
+      </div>
+
+    </div>
   );
-  }
+}
 
 export default Domain;
