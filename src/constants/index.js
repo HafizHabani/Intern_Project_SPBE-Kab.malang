d900938d212be1c0ca45cd3b404ed9aa952e954c
Irfan Rosyadi <screetgz@gmail.com>
2024-03-24 19:09:03 +0700

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 import { facebook, instagram, linkedin, twitter, } from "../assets";
 import axios from "axios"
-const baseUrl = 'http://127.0.0.1:8000/api/';
+const baseUrl = 'https://fri-mega-submission-arbitration.trycloudflare.com/api/';
 
 export const navLinks = [
   {
